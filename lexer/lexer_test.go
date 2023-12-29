395df9b38aa965ab3bbf4a8ece66806d1510fc37
Jorghy-Del <91205677+Jorghy-Del@users.noreply.github.com>
2023-12-29 22:58:40 +0530

    write tests

---
+++ lexer/lexer_test.go
@@ -0,0 +1,30 @@
+package lexer
+
+import (
+	"github.com/Jorghy-Del/gorth/lexer"
+	"github.com/Jorghy-Del/gorth/word"
+	"testing"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `5 10 +`
+
+	tests := []struct {
+		expectedType    word.WordType
+		expectedLiteral string
+	}{
+		{word.PUSH, "5"},
+		{word.PUSH, "10"},
+		{word.ADD, "+"},
+	}
+	l := lexer.New(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Literal)
+			if tok.Literal != tt.expectedLiteral {
+				t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+			}
+		}
+	}
+}
