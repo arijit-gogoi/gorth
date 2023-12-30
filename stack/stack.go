606047ed9573862a284618a0d87869416d2883c6
Jorghy-Del <91205677+Jorghy-Del@users.noreply.github.com>
2023-12-30 09:12:33 +0530

    export the Stk field

--- stack/stack.go
+++ stack/stack.go
@@ -1,19 +1,19 @@
 package stack
 
 type Stack struct {
-	stack []int
+	Stk []int
 }
 
 func (s *Stack) Push(v int) {
-	s.stack = append(s.stack, v)
+	s.Stk = append(s.Stk, v)
 }
 
 func (s *Stack) Pop() (top int) {
-	top = s.stack[len(s.stack)-1]
-	s.stack = s.stack[:len(s.stack)-1]
+	top = s.Stk[len(s.Stk)-1]
+	s.Stk = s.Stk[:len(s.Stk)-1]
 	return top
 }
 
 func (s *Stack) Len() int {
-	return len(s.stack)
+	return len(s.Stk)
 }
