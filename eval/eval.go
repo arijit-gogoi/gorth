b3be1d2e76136b2129481713d954fe8b315c7684
Jorghy-Del <91205677+Jorghy-Del@users.noreply.github.com>
2023-12-31 09:36:50 +0530

    expand evaluator and more tests

--- eval/eval.go
+++ eval/eval.go
@@ -9,7 +9,6 @@ import (
 	"github.com/Jorghy-Del/gorth/word"
 )
 
-
 func eval(words []word.Word) []int {
 	var s stack.Stack
 	for _, w := range words {
@@ -18,15 +17,55 @@ func eval(words []word.Word) []int {
 			v1 := s.Pop()
 			v2 := s.Pop()
 			s.Push(v1 + v2)
+		case word.SUBTRACT:
+			v1 := s.Pop()
+			v2 := s.Pop()
+			s.Push(v1 - v2)
+		case word.MULTIPLY:
+			v1 := s.Pop()
+			v2 := s.Pop()
+			s.Push(v1 * v2)
+		case word.DIVIDE:
+			v1 := s.Pop()
+			v2 := s.Pop()
+			s.Push(v1 / v2)
 		case word.POP:
 			top := s.Pop()
 			fmt.Println(top)
+		case word.DUP:
+			top := s.Top()
+			s.Push(top)
+		case word.DROP:
+			s.Pop()
+		case word.SWAP:
+			first := s.Pop()
+			second := s.Pop()
+			s.Push(first)
+			s.Push(second)
+		case word.OVER:
+			sec := s.Second()
+			s.Push(sec)
+		case word.SPIN:
+			n1 := s.Pop()
+			n2 := s.Pop()
+			n3 := s.Pop()
+			s.Push(n2)
+			s.Push(n3)
+			s.Push(n1)
+		case word.EMIT:
+			n := s.Pop()
+			fmt.Println(string(rune(n)))
+		case word.CR:
+			fmt.Println()
 		case word.PUSH:
 			v, e := strconv.Atoi(w.Literal)
 			if e != nil {
 				log.Fatal(e)
 			}
 			s.Push(v)
+		default:
+			fmt.Println("you reached default")
+			log.Fatal("You reached DEFAULT")
 		}
 	}
 	return s.Stk
--- eval/eval_test.go
+++ eval/eval_test.go
@@ -3,39 +3,149 @@ package eval
 import (
 	"testing"
 
-	"github.com/Jorghy-Del/gorth/word"
 	"github.com/Jorghy-Del/gorth/lexer"
+	"github.com/Jorghy-Del/gorth/word"
 )
 
 func TestEval(t *testing.T) {
-	input := `5 10 + . 1`
-	tests := []struct {
+	input := `5 -10 + 1 . 1 - -1 * -6 / dup drop 2 swap over spin . . . 97 emit`
+	type expected struct {
 		expectedType    word.WordType
 		expectedLiteral string
-		expectedStk []int
-	}{
+		expectedStk     []int
+	}
+	output := []expected{
 		{word.PUSH, "5", []int{5}},
-		{word.PUSH, "10", []int{5, 10}},
-		{word.ADD, "+", []int{15}},
+		{word.PUSH, "-10", []int{5, -10}},
+		{word.ADD, "+", []int{-5}},
+		{word.PUSH, "1", []int{-5, 1}},
+		{word.POP, ".", []int{-5}},
+		{word.PUSH, "1", []int{-5, 1}},
+		{word.SUBTRACT, "-", []int{6}},
+		{word.PUSH, "-1", []int{6, -1}},
+		{word.MULTIPLY, "*", []int{-6}},
+		{word.PUSH, "-6", []int{-6, -6}},
+		{word.DIVIDE, "/", []int{1}},
+		{word.DUP, "dup", []int{1, 1}},
+		{word.DROP, "drop", []int{1}},
+		{word.PUSH, "2", []int{1, 2}},
+		{word.SWAP, "swap", []int{2, 1}},
+		{word.OVER, "over", []int{2, 1, 2}},
+		{word.SPIN, "spin", []int{1, 2, 2}},
+		{word.POP, ".", []int{1, 2}},
+		{word.POP, ".", []int{1}},
 		{word.POP, ".", []int{}},
-		{word.PUSH, "1", []int{1}},
+		{word.PUSH, "97", []int{97}},
+		{word.EMIT, "emit", []int{}},
 	}
 	l := lexer.New(input)
 	words := []word.Word{}
-	for i, tt := range tests {
+	for i, tt := range output {
 		tok := l.NextToken()
 		words = append(words, tok)
 		got := eval(words)
 		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Literal)
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
 		}
 		if tok.Literal != tt.expectedLiteral {
 			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
 		}
 		for j := range got {
 			if got[j] != tt.expectedStk[j] {
-				t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+				t.Fatalf("tests[%d] - wrong evaluation. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
 			}
+		}
+	}
+}
+
+func TestEvalTable(t *testing.T) {
+	type expected struct {
+		expectedType    word.WordType
+		expectedLiteral string
+		expectedStk     []int
+	}
+	type test struct {
+		name 	string
+		input  	string
+		output 	[]expected
+	}
+	tests := []test{
+		{
+			name: "add one and minus one",
+			input: `1 -1 +`,
+			output:
+			[]expected{
+				{word.PUSH, "1", []int{1}},
+				{word.PUSH, "-1", []int{1, -1}},
+				{word.ADD, "+", []int{0}},
+			},
+		},
+		{
+			name: "subtract two from one",
+			input: `2 1 -`,
+			output:
+			[]expected{
+				{word.PUSH, "2", []int{2}},
+				{word.PUSH, "1", []int{2, 1}},
+				{word.SUBTRACT, "-", []int{-1}},
+			},
+		},
+		{
+			name: "dup a number",
+			input: `420 dup`,
+			output: []expected{
+				{word.PUSH, "420", []int{420}},
+				{word.DUP, "dup", []int{420, 420}},
+			},
+		},
+		{
+			name: "cr cr cr",
+			input: `cr cr cr`,
+			output: []expected{
+				{word.CR, "cr", []int{}},
+				{word.CR, "cr", []int{}},
+				{word.CR, "cr", []int{}},
+			},
+		},
+		{
+			name: "1 2 3 cr cr cr",
+			input: `1 2 3 cr cr cr`,
+			output: []expected{
+				{word.PUSH, "1", []int{1}},
+				{word.PUSH, "2", []int{1, 2}},
+				{word.PUSH, "3", []int{1, 2, 3}},
+				{word.CR, "cr", []int{1, 2, 3}},
+				{word.CR, "cr", []int{1, 2, 3}},
+				{word.CR, "cr", []int{1, 2, 3}},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		l := lexer.New(tc.input)
+		words := []word.Word{}
+
+		for i, o := range tc.output {
+			tok := l.NextToken()
+			words = append(words, tok)
+			got := eval(words)
+			t.Run(tc.name, func(t *testing.T) {
+				if tok.Type != o.expectedType {
+					t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, o.expectedType, tok.Type)
+				}
+			})
+			t.Run(tc.name, func(t *testing.T) {
+				if tok.Literal != o.expectedLiteral {
+					t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, o.expectedLiteral, tok.Literal)
+				}
+			})
+			t.Run(tc.name, func(t *testing.T) {
+				for j := range got {
+					if got[j] != o.expectedStk[j] {
+						t.Fatalf("tests[%d] - wrong evaluation. expected=%q, got=%q", i, o.expectedLiteral, tok.Literal)
+					}
+				}
+			})
 		}
 	}
 }
